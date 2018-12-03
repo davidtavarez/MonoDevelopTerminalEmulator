a61ae02fb5d2e2d5ef6bcda2bf396d498768b6ca
David Tavarez <davidftavarez@gmail.com>
2018-12-03 09:23:23 -0400

    VTE

---
+++ MonoDevelopTerminalEmulator/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "TerminalEmulator",
+    Namespace = "MonoDevelopTerminalEmulator",
+    Version = "1.0"
+)]
+
+[assembly: AddinName("TerminalEmulator")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("Open a Terminal inside the IDE")]
+[assembly: AddinAuthor("David Tavarez")]
+
+[assembly: ImportAddinAssembly("vte-sharp.dll")]
---
+++ MonoDevelopTerminalEmulator/TerminalEmulatorPad.cs
@@ -0,0 +1,75 @@
+﻿using MonoDevelop.Ide.Gui;
+
+using Gtk;
+using MonoDevelop.Components;
+using System;
+using System.Collections;
+
+namespace MonoDevelopTerminalEmulator
+{
+    public class TerminalEmulatorPad : PadContent
+    {
+        Vte.Terminal control;
+
+        public TerminalEmulatorPad()
+        {
+        }
+
+        public TerminalEmulatorPad(string title, string icon = null) : base(title, icon)
+        {
+        }
+
+        public override Control Control
+        {
+            get
+            {
+                if (control == null)
+                    CreateControl();
+                return control;
+            }
+        }
+        public string DefaultPlacement => "Bottom";
+
+        public void CreateControl()
+        {
+            control = new Vte.Terminal
+            {
+                CursorBlinks = true,
+                MouseAutohide = true,
+                ScrollOnKeystroke = true,
+                DeleteBinding = Vte.TerminalEraseBinding.Auto,
+                BackspaceBinding = Vte.TerminalEraseBinding.Auto,
+                Encoding = "UTF-8",
+            };
+
+            string[] argv = Environment.GetCommandLineArgs();
+
+            string[] envv = new string[Environment.GetEnvironmentVariables().Count];
+            int i = 0;
+            foreach (DictionaryEntry e in Environment.GetEnvironmentVariables())
+            {
+                if (e.Key == string.Empty || e.Value == string.Empty)
+                {
+                    continue;
+                }
+
+                string tmp = String.Format("{0}={1}", e.Key, e.Value);
+                envv[i] = tmp;
+                i++;
+            }
+
+            int pid = control.ForkCommand(
+                Environment.GetEnvironmentVariable("SHELL"),
+                argv,
+                envv,
+                Environment.CurrentDirectory,
+                false,
+                true,
+                true);
+
+            Console.WriteLine("Child pid: {0}", pid);
+
+            control.Show();
+        }
+    }
+}
