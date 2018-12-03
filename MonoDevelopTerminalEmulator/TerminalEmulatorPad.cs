4e73e6503546f1f4dfcab32784c3736454d27920
David Tavarez <davidftavarez@gmail.com>
2018-12-03 14:28:38 -0400

    VTE.Terminal as the main Control

--- MonoDevelopTerminalEmulator/TerminalEmulatorPad.cs
+++ MonoDevelopTerminalEmulator/TerminalEmulatorPad.cs
@@ -9,30 +9,29 @@ namespace MonoDevelopTerminalEmulator
 {
     public class TerminalEmulatorPad : PadContent
     {
-        Vte.Terminal control;
-
-        public TerminalEmulatorPad()
-        {
-        }
-
-        public TerminalEmulatorPad(string title, string icon = null) : base(title, icon)
-        {
-        }
+        Vte.Terminal terminal;
 
         public override Control Control
         {
             get
             {
-                if (control == null)
+                if (terminal == null)
                     CreateControl();
-                return control;
+                return terminal;
             }
         }
+
         public string DefaultPlacement => "Bottom";
 
-        public void CreateControl()
+        void CreateControl()
         {
-            control = new Vte.Terminal
+            Gdk.Color white = new Gdk.Color();
+            Gdk.Color.Parse("white", ref white);
+
+            Gdk.Color black = new Gdk.Color();
+            Gdk.Color.Parse("black", ref black);
+
+            terminal = new Vte.Terminal
             {
                 CursorBlinks = true,
                 MouseAutohide = true,
@@ -40,36 +39,32 @@ namespace MonoDevelopTerminalEmulator
                 DeleteBinding = Vte.TerminalEraseBinding.Auto,
                 BackspaceBinding = Vte.TerminalEraseBinding.Auto,
                 Encoding = "UTF-8",
+                Visible = true,
+                IsFocus = true,
+                BackgroundTintColor = black,
             };
-
-            string[] argv = Environment.GetCommandLineArgs();
 
             string[] envv = new string[Environment.GetEnvironmentVariables().Count];
             int i = 0;
             foreach (DictionaryEntry e in Environment.GetEnvironmentVariables())
             {
-                if (e.Key == string.Empty || e.Value == string.Empty)
+                if ((string)e.Key == string.Empty ||
+                    (string)e.Value == string.Empty)
                 {
                     continue;
                 }
-
                 string tmp = String.Format("{0}={1}", e.Key, e.Value);
                 envv[i] = tmp;
                 i++;
             }
 
-            int pid = control.ForkCommand(
-                Environment.GetEnvironmentVariable("SHELL"),
-                argv,
-                envv,
-                Environment.CurrentDirectory,
-                false,
-                true,
-                true);
-
-            Console.WriteLine("Child pid: {0}", pid);
-
-            control.Show();
+            terminal.ForkCommand(Environment.GetEnvironmentVariable("SHELL"),
+                                new string[0],
+                                envv,
+                                Environment.CurrentDirectory,
+                                false,
+                                true,
+                                true);
         }
     }
 }
